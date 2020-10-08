154d53fb7feb34453203d9b7a522f5b8ffe0dd8e
ReedBen <60700716+ReedBen@users.noreply.github.com>
2020-10-08 11:23:10 +0100

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,17 +1,35 @@
-import requests
+import json
+import os
+import weatherconstants
 
-url = 'http://api.openweathermap.org/data/2.5/weather?lat=51.6708&lon=-1.2880&appid=e80696a64fffd6b3a896d7ce2ced768f&units=metric'
-res = requests.get(url)
-data = res.json()
+def history():
+    weather_history = {}
+    history_dir = weatherconstants.get_history_dir()
 
-temperature = data['main']['temp']
-pressure = data['main']['pressure']
-humidity = data['main']['humidity']
-wind_speed = data['wind']['speed']
-wind_direction = data['wind']['deg']
+    for weather_file in os.listdir(history_dir):
+        with open(history_dir+weather_file, 'r') as weather_file:
+                txt = weather_file.read()
+                js = json.loads(txt)
+                for hour_data in js['hourly']:
+                    if not hour_data['dt'] in weather_history:
+                        weather_history[hour_data['dt']] = hour_data
+    return weather_history
 
-print('Temperature : {} °C'.format(temperature))
-print('Air pressure: {} Pa'.format(pressure))
-print('Humidity: {} %'.format(humidity))
-print('Wind Speed: {} m/s'.format(wind_speed))
-print('Wind Direction: {} °'.format(wind_direction))
+def forecasts():
+    weather_forecasts = {}
+    forecast_dir = weatherconstants.get_forecast_dir()
+
+    for weather_file in os.listdir(forecast_dir):
+        with open(forecast_dir+weather_file, 'r') as weather_file:
+            txt = weather_file.read()
+            js = json.loads(txt)
+            first_dt = js['hourly'][0]['dt']
+            if not first_dt in weather_forecasts:
+                weather_forecasts[first_dt] = js['hourly']
+
+    return weather_forecasts
+
+if __name__ == '__main__':
+    print(history())
+    print(forecasts())
+
