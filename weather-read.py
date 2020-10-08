9266faf270ca4f7dca0a98008054ded121cdf183
ReedBen <60700716+ReedBen@users.noreply.github.com>
2020-10-08 11:27:24 +0100

    Delete weather-read.py

--- weather-read.py
+++
@@ -1,7 +0,0 @@
-import weather
-history = weather.history()
-forecasts = weather.forecasts()
-
-for forecast_time in forecasts:
-    if forecast_time in history:
-        print('%f - %f' % (history[forecast_time]['temp'], forecasts[forecast_time][0]['temp']))
